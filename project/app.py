7bd71a3579b5906c69490b46b1f1f99832df88a7
gas53 <gas53@bk.ru>
2023-05-27 09:48:48 +0300

    dz10 is_over

--- project/app.py
+++ project/app.py
@@ -2,19 +2,30 @@ from flask import Flask
 from security import flask_bcrypt
 from flask_combo_jsonapi import Api
 
+
+from config import ProductionConfig, DevelopmentConfig
+from api.tag import TagDetail, TagList, UserList, UserDetail
+from apispec import APISpec
+from apispec.ext.marshmallow import MarshmallowPlugin
+from flask_apispec.extension import FlaskApiSpec
+from combojsonapi.event import EventPlugin
+from combojsonapi.permission import PermissionPlugin
+
 from user.autor import authors
 from article.views import article
 from user.views import user
 from user.auth import auth, login_manager
 from admin import admin
 from models.database import db
-from config import ProductionConfig, DevelopmentConfig
-from api.tag import TagDetail, TagList, UserList, UserDetail
-
-
+from user.autor import authors_list
 
 def registr_api(app: Flask):
-    api = Api(app)
+    event_plugin = EventPlugin()
+    permission_plugin = PermissionPlugin(strict=False)
+    api = Api(app, plugins=[
+            event_plugin,
+            permission_plugin,
+            ],)
     api.route(UserList, "user_list", "/api/1.0/users/")
     api.route(UserDetail, "user_detail", "/api/1.0/users/<int:id>/")
     api.route(TagList, "author_list", "/api/1.0/tags/" )
@@ -36,8 +47,18 @@ def create_app():
     db.init_app(app)
     admin.init_app(app)
     registr_api(app)
-    # app.route(TagList, "author_list", "/api/1.0/authors/" )
-    # app.route(TagDetail, "author_detail", "/api/1.0/authors/<int:id>/")
+    app.config.update({
+        'APISPEC_SPEC': APISpec(
+            title='pets',
+            version=2,
+            openapi_version=2,
+            plugins=[MarshmallowPlugin()],
+        ),
+        'APISPEC_SWAGGER_URL': '/swagger/',
+    })
+    docs = FlaskApiSpec(app)
+
+    docs.register(authors_list)
 
 
     return app
